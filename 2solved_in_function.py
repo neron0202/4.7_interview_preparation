c0080d4c2b83ca8ed9dc8ffd00cebbf02a7cd098
Daniil/Pecherin <daniel.pechyorin@mail.ru>
2021-11-01 00:48:48 +0300

    3rd revision

--- 2solved_in_function.py
+++ 2solved_in_function.py
@@ -10,30 +10,21 @@ def create_open_brackets_list():
     for el in brackets_line:
         if el == '(' or el == '[' or el == '{':
             stack_list.push(el)
-            print("stack_list= ", stack_list)
+            print("stack_list= ", stack_list.el_list)
         elif el == ')' or el == ']' or el == '}':
-            if not stack_list:
-                return f"Несбалансировано. "
-            if not el:
-                return f"Несбалансировано. "
-            if stack_list:
+            if stack_list.isEmpty():
+                return f"Несбалансировано. size = 0"
+            elif stack_list:
                 open_bracket = stack_list.pop()
-                print("open_bracket= ", open_bracket)
-            else:
-                return f"Несбалансировано. el= {el}"
-            if open_bracket == "(" and el == ")":
-                continue
-            if open_bracket == "[" and el == "]":
-                continue
-            if open_bracket == "{" and el == "}":
-                continue
-            if not el:
-                return f"Несбалансировано. "
-            else:
-                return f"Несбалансировано. el= {el}"
+                if open_bracket == "(" and el == ")":
+                    continue
+                if open_bracket == "[" and el == "]":
+                    continue
+                if open_bracket == "{" and el == "}":
+                    continue
     if stack_list:
         return "Несбалансировано"
     return "Сбалансировано"
 
 
-create_open_brackets_list()
+print(create_open_brackets_list())
